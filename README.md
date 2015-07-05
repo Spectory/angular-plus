0650d7cf1b96d818a4f094b6f3299c1ace64590a
= <assaf@shiftz.com>
2015-07-05 10:43:24 +0300

    awesome-if

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # angular-plus
+
+## Angular awesome-if
+
+```
+<include-directive 
+  awesome-if="should_show" 
+  template="platforms/connections"> <!-- rails template -->
+</include-directive>
+```
+
+awesome-if is a directive that behaves like ng-show, but removes the dom while
+not shown(i.e detaches it). It's the same as ng-if, but without the overhead of recompiling largs dom each time.
+It's not recreated and as such, to be aware that your controller has been reshown use the following:
+
+```
+  $scope.$parent.restart = function () {
+    $scope.init();
+  };
+
+  $scope.$parent.stop = function () {
+    ...
+  };  
+```
+
+
---
+++ awesome_if.js
@@ -0,0 +1,76 @@
+/*global angular, unused */
+
+angular.module('spectory').directive('awesomeIf', [function () {
+  'use strict';
+  return {
+    multiElement: true,
+    transclude: 'element',
+    priority: 600,
+    terminal: true,
+    restrict: 'A',
+    $$tlb: true,
+    link: function ($scope, $element, $attr, ctrlr, $transclude) {
+      angular.noop(ctrlr);
+
+      var block, childScope;
+      function shouldReuseDom() {
+        if (block) {
+          return true;
+        }
+
+        return false;
+      }
+
+      function reuseDom() {
+        if (childScope.restart) {
+          childScope.restart();
+        }
+        $element.after(block.clone);
+      }
+
+      function removeButCacheDom() {
+        block.clone[0].parentNode.removeChild(block.clone[0]);
+        if (childScope.stop) {
+          childScope.stop();
+        }
+      }
+
+      function isDomCreated() {
+        if (block) {
+          return true;
+        }
+
+        return false;
+      }
+
+      function createDom() {
+        $transclude(function (clone, newScope) {
+          childScope = newScope;
+          block = {
+            clone: clone
+          };
+
+          $element.after(block.clone);
+
+        });
+      }
+
+      $scope.$watch($attr.awesomeIf, function awesomeIfWatchAction(value) {
+        if (value) {
+          if (!childScope || block) {
+            if (shouldReuseDom()) {
+              reuseDom();
+              return;
+            }
+
+            createDom();
+          }
+        } else {
+          if (isDomCreated()) {
+            removeButCacheDom();
+          }
+        }
+      });
+    }
+  };
+}]);
